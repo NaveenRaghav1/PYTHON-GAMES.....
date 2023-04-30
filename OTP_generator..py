04ca57032f04a24bcc9a438200d088143b2e3f2e
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-04-30 19:43:39 +0530

    Add files via upload
    this  a code for genrating a  OTP code

---
+++ OTP_generator..py
@@ -0,0 +1,13 @@
+import random as r
+name=input("enter your name please-->\n")
+print("-------------------------------------------")
+print('''           
+      -🙏🙏-------WELCOME-------🙏🙏-
+                    ''')
+print(name,'! Lets begin the game...😊😊 ')
+print("-------------------------------------------")
+import random as r
+
+out = r.random()
+
+print(str(out)[-4:])
